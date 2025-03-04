87fb930e7289e783a09f2a74b543cce67b9f01b9
aniya <fyeasmin1@fordham.edu>
2025-03-04 16:58:46 -0500

    opening of game

---
+++ 2d-game/control.gd
@@ -0,0 +1,9 @@
+extends Control
+
+# Called when the "Start" button is pressed
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://GameScene.tscn")  # Change to your actual game scene
+
+# Called when the "Quit" button is pressed
+func _on_quit_button_pressed():
+	get_tree().quit()  # Quit the game
