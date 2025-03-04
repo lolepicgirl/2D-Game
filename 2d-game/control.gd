8136b59fe1a41da5e9e90673fdf6e8f23db87e34
aniya <fyeasmin1@fordham.edu>
2025-03-04 17:49:11 -0500

    Button Update 2.0

--- 2d-game/control.gd
+++ 2d-game/control.gd
@@ -7,3 +7,11 @@ func _on_start_button_pressed():
 # Called when the "How to Play" button is pressed
 func _on_how_to_play_button_pressed():
 	get_tree().change_scene_to_file("res://HowToPlayScene.tscn")  # Change to your actual How To Play scene
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	pass # Replace with function body.
