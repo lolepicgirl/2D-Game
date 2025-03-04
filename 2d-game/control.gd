4e82fbed1d4214adab8b49dc765be6df80c2f3d9
aniya <fyeasmin1@fordham.edu>
2025-03-04 17:43:48 -0500

    Button Update

--- 2d-game/control.gd
+++ 2d-game/control.gd
@@ -4,6 +4,6 @@ extends Control
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")  # Change to your actual game scene
 
-# Called when the "Quit" button is pressed
-func _on_quit_button_pressed():
-	get_tree().quit()  # Quit the game
+# Called when the "How to Play" button is pressed
+func _on_how_to_play_button_pressed():
+	get_tree().change_scene_to_file("res://HowToPlayScene.tscn")  # Change to your actual How To Play scene
