11f33c04a64d28e338ec0f3790bff40c1e728f25
cat <cathapple5401@gmail.com>
2025-03-14 11:54:41 -0400

    portal fix?

--- 2d-game/portal.gd
+++ 2d-game/portal.gd
@@ -12,5 +12,5 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_packed(nextscene)
+	get_tree().change_scene_to_file("res://2D-Game/2d-game/1st Floor.tscn")
 	
