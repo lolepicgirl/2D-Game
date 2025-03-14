2c6edf1745136766839f1a175a60b210aa5d8049
cat <cathapple5401@gmail.com>
2025-03-14 13:10:17 -0400

    updates

--- 2d-game/2d-game/2d-game/camera_2d.gd
+++ 2d-game/2d-game/2d-game/camera_2d.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 
-#@export var target: Landing Page  # Drag your child node here in the Inspector
+@export var target: Node2D
 
-#func _process(delta):
-	#if target:
-		#global_position = target.global_position
+func _process(delta):
+	if target:
+		global_position = target.global_position
