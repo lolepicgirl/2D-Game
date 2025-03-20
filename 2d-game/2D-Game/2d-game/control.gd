fb60489a3ceb7aa01089b304a4629801f9c2b5b7
naswima <nakter5@fordham.edu>
2025-03-19 22:49:39 -0400

    HowToPlay.tscn & Buttons Update

--- 2d-game/2D-Game/2d-game/control.gd
+++ 2d-game/2D-Game/2d-game/control.gd
@@ -1,8 +1,8 @@
 extends Control
 
 func _on_how_to_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://castlefacade.tscn")
+	get_tree().change_scene_to_file("res://HowToPlay.tscn")
 
 
 func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://castlefacade.tscn")
+	get_tree().change_scene_to_file("res://HowToPlay.tscn")
